79f89de96a45a3f8b99f89c1e9cd67d33fb83c66
comfouriertech <comfouriertech@comfouriertechdemac-mini.local>
2016-08-10 16:58:44 +0800

    Initial Commit

---
+++ CustomCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomCamera
+//
+//  Created by comfouriertech on 16/8/10.
+//  Copyright © 2016年 ronghua_li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomCamera/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CustomCamera
+//
+//  Created by comfouriertech on 16/8/10.
+//  Copyright © 2016年 ronghua_li. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
