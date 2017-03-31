80bea6e1969b0f884ec1e7196396cd3c07c535b5
manguoge <312825660@qq.com>
2017-03-31 14:46:54 +0800

    fix some bug

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomCamera
+//
+//  Created by comfouriertech on 16/8/10.
+//  Copyright © 2016年 ronghua_li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
